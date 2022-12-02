6955dd4bf0d3e0b77b7a12bce052e0b4c4cc0d7b
taniatdtat <104831305+taniatdtat@users.noreply.github.com>
2022-12-02 15:14:08 +0300

    Update dec1.py

--- hw_8/dec1.py
+++ hw_8/dec1.py
@@ -1,5 +1,5 @@
-def dec_1 (foo):
-    def wrapper (*args,**kwargs):
+def dec_reverse_args(foo):    #разворачиваем аргументы
+    def wrapper (*args):
         arr = []
         for x in args:
             arr.append(x)
@@ -7,10 +7,41 @@ def dec_1 (foo):
         return (foo(*arr))
     return wrapper
 
-@dec_1
-def foo(*args):
-    return args
 
-print (foo(1,2,3,4,5,"adv"))
+def dec_print_args (foo):
+    def wrapper (*args):
+        foo(*args)
+        print(*args)
+    return wrapper
+
+def dec_error (foo):
+    def wrapper (*args):
+        try:
+            foo(*args)
+        except Exception:
+            print ("Error")
+    return wrapper
+
+@dec_reverse_args
+def foo_1(*args):
+    print(*args)
+
+@dec_print_args
+def foo_2(*args):
+    sum = 0
+    for x in args:
+        sum +=x
+    print(x)
+    
+@dec_error
+def foo_3 (*args):
+    print(args[0]*args[1])
+
+
+
+foo_1(1,2,3,4,5,"adv")
+foo_2( 1,3,5,7)
+foo_3( "b","a")
+
             
     
